572162035b1d20663fc9a98adf2154d9a6015fb2
Mohit Gambhir <mohit.gambhir@intel.com>
2014-06-26 00:50:03 -0400

    Changed the license on the sources from Intel GPL to Intel BSD

--- lib/libnullptlib/include/asim/ptlib.h
+++ lib/libnullptlib/include/asim/ptlib.h
@@ -1,16 +1,31 @@
 /**************************************************************************
- * INTEL CONFIDENTIAL Copyright (c) 2007 Intel Corp.
+ * Copyright (c) 2014, Intel Corporation
  *
- * Recipient is granted a non-sublicensable copyright license under
- * Intel copyrights to copy and distribute this code internally only.
- * This code is provided "AS IS" with no support and with no
- * warranties of any kind, including warranties of MERCHANTABILITY,
- * FITNESS FOR ANY PARTICULAR PURPOSE or INTELLECTUAL PROPERTY
- * INFRINGEMENT. By making any use of this code, Recipient agrees that
- * no other licenses to any Intel patents, trade secrets, copyrights
- * or other intellectual property rights are granted herein, and no
- * other licenses shall arise by estoppel, implication or by operation
- * of law. Recipient accepts all risks of use.
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright 
+ *   notice, this list of conditions and the following disclaimer in the 
+ *   documentation and/or other materials provided with the distribution.
+ * - Neither the name of the Intel Corporation nor the names of its 
+ *   contributors may be used to endorse or promote products derived from 
+ *   this software without specific prior written permission.
+ *  
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **************************************************************************/
 
 #ifndef _PTLIB__H_
